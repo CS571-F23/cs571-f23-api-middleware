aefe4e870441be71091262dabc7f5901f4df4de2
Cole Nelson <ctnelson1997@gmail.com>
2023-08-10 11:11:01 -0500

    working pub conf

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Welcome to the CS571 F23 API Middleware!");
+import { CS571Config } from "./model/config";
+import { CS571Configurator } from "./services/configure";
+import { CS571Initializer } from "./services/initialize";
+
+export default { CS571Configurator, CS571Initializer, CS571Config } 
